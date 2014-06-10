27be636c9a11d1bff304e1729a168de8ef3952ce
Matt Shirley <mdshw5@gmail.com>
2014-06-10 14:21:49 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='pyfaidx',
     provides='pyfaidx',
-    version='0.1.6',
+    version='0.1.7',
     author='Matthew Shirley',
     author_email='mdshw5@gmail.com',
     url='http://mattshirley.com',
