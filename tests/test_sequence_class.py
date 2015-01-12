bae0a9f6cdc1786192ef1a9ac3027be1b2d0934d
Matt Shirley <mdshw5@gmail.com>
2015-01-12 15:14:43 -0500

    Try to detect non-DNA FASTA entries and prevent complement method from running. Added tests for invalid sequence.

--- tests/test_sequence_class.py
+++ tests/test_sequence_class.py
@@ -1,6 +1,10 @@
 from pyfaidx import Sequence
+from nose.tools import assert_raises
 
 test_seq = Sequence(name='KF435150.1', seq='TTGAAGATTTTGCATGCAGCAGGTGCGCAAGGTGAAATGTTCACTGTTAAA',
+                    start=100, end=150)
+
+test_seq_invalid = Sequence(name='KF435150.1', seq='TTGAAGATTTPGCATGCAGCAGGTGCGCAAGGTGAAATGTTCACTGTTAAA',
                     start=100, end=150)
 
 def test_negate():
@@ -10,3 +14,6 @@ def test_negate_metadata():
     # Negate should affect __repr__ the same way as reverse and complement
     test_seq_neg = -test_seq
     assert test_seq_neg.__repr__() == test_seq.complement[::-1].__repr__()
+
+def test_complement_invalid():
+    assert_raises(ValueError, lambda: test_seq_invalid.complement)
