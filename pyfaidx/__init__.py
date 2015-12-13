b8da2625e851c1e801b2a6b0f85773a073f84248
Matt Shirley <mdshw5@gmail.com>
2015-12-13 15:25:31 -0500

    Bump version for release.

--- pyfaidx/__init__.py
+++ pyfaidx/__init__.py
@@ -21,7 +21,7 @@ from math import ceil
 
 dna_bases = re.compile(r'([ACTGNactgnYRWSKMDVHBXyrwskmdvhbx]+)')
 
-__version__ = '0.4.7'
+__version__ = '0.4.7.1'
 
 
 class FastaIndexingError(Exception):
