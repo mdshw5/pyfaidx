ad0d3fb62819a924f512952e82076d6a0068e8b8
Matt Shirley <mdshw5@gmail.com>
2016-10-19 20:18:51 -0400

    Update __init__.py

--- pyfaidx/__init__.py
+++ pyfaidx/__init__.py
@@ -22,7 +22,7 @@ from threading import Lock
 
 dna_bases = re.compile(r'([ACTGNactgnYRWSKMDVHBXyrwskmdvhbx]+)')
 
-__version__ = '0.4.8'
+__version__ = '0.4.8.1'
 
 
 class FastaIndexingError(Exception):
