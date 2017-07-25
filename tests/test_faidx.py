066b9ef37ffef499e68140bbef000e12708c6bef
Matt Shirley <mdshw5@gmail.com>
2017-07-25 13:16:14 -0400

    Add tests for #120

--- tests/test_faidx.py
+++ tests/test_faidx.py
@@ -1,8 +1,10 @@
 import os
+import filecmp
 from pyfaidx import FastaIndexingError, BedError, FetchError
 from pyfaidx.cli import main
 from nose.tools import raises
 from unittest import TestCase
+from tempfile import NamedTemporaryFile
 
 path = os.path.dirname(__file__)
 os.chdir(path)
@@ -36,3 +38,12 @@ class TestCLI(TestCase):
         
     def test_key_warning(self):
         main(['data/genes.fasta', 'foo'])
+        
+    def test_auto_strand(self):
+        """ Test that --auto-strand produces the same output as --reverse --complement"""
+        with NamedTemporaryFile() as auto_strand:
+            with NamedTemporaryFile() as noto_strand:
+                main(['--auto-strand', '-o', auto_strand.name, data/genes.fasta', 'gi|557361099|gb|KF435150.1|:100-1'])
+                main(['--reverse', '--complement', '-o', noto_strand.name, data/genes.fasta', 'gi|557361099|gb|KF435150.1|:1-100'])
+                self.assertTrue(filecmp.cmp(auto_strand.name, noto_strand.name))
+        
