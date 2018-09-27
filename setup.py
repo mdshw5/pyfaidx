b883b93cd348a83431447c319a49a5598bc7c68d
Ubuntu <ubuntu@ip-10-2-72-208.us-west-2.compute.internal>
2018-09-27 16:56:36 +0000

    add encoding option

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 install_requires = ['six', 'setuptools >= 0.7', 'smart_open >= 1.7.1']
 if sys.version_info[0] == 2 and sys.version_info[1] == 6:
-    install_requires.extend(['ordereddict', 'argparse'])
+    install_requires.extend(['ordereddict', 'argparse', 'importlib'])
 
 
 def get_version(string):
