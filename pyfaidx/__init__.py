22579e6f092b3c31a6a1f65a9ec4aebe7b1fbf11
Matt Shirley <mdshw5@gmail.com>
2018-10-26 22:25:29 -0400

    bump version which fixes #145

--- pyfaidx/__init__.py
+++ pyfaidx/__init__.py
@@ -25,7 +25,7 @@ if sys.version_info > (3, ):
 
 dna_bases = re.compile(r'([ACTGNactgnYRWSKMDVHBXyrwskmdvhbx]+)')
 
-__version__ = '0.5.5.1'
+__version__ = '0.5.5.2'
 
 
 class KeyFunctionError(ValueError):
