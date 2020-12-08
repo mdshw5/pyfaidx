30b9e16f2c4f727de3f21782f7edf37edb38bdbe
Matt Shirley <mdshw5@gmail.com>
2020-12-08 18:07:05 -0500

    Bump version for release

--- pyfaidx/__init__.py
+++ pyfaidx/__init__.py
@@ -29,7 +29,7 @@ if sys.version_info > (3, ):
 
 dna_bases = re.compile(r'([ACTGNactgnYRWSKMDVHBXyrwskmdvhbx]+)')
 
-__version__ = '0.5.9.1'
+__version__ = '0.5.9.2'
 
 
 class KeyFunctionError(ValueError):
