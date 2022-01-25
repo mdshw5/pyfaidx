174184f154baf8c15bfa5fb833275e537ee9f7f5
Matt Shirley <mdshw5@gmail.com>
2022-01-24 19:44:03 -0500

    Universal newlines became standard in python3

--- tests/test_bio_seqio.py
+++ tests/test_bio_seqio.py
@@ -21,19 +21,19 @@ class TestBioSeqIO(TestCase):
 
     def test_fetch_whole_entry(self):
         fasta = Fasta('data/genes.fasta')
-        with open('data/genes.fasta', "rU") as fh:
+        with open('data/genes.fasta', "r") as fh:
             seqio = SeqIO.to_dict(SeqIO.parse(fh, "fasta"))
         assert str(fasta['gi|557361099|gb|KF435150.1|']) == str(seqio['gi|557361099|gb|KF435150.1|'].seq)
         assert fasta['gi|557361099|gb|KF435150.1|'].name == str(seqio['gi|557361099|gb|KF435150.1|'].name)
 
     def test_slice_whole_entry(self):
         fasta = Fasta('data/genes.fasta')
-        with open('data/genes.fasta', "rU") as fh:
+        with open('data/genes.fasta', "r") as fh:
             seqio = SeqIO.to_dict(SeqIO.parse(fh, "fasta"))
         assert str(fasta['gi|557361099|gb|KF435150.1|'][::3]) == str(seqio['gi|557361099|gb|KF435150.1|'].seq[::3])
 
     def test_revcomp_whole_entry(self):
         fasta = Fasta('data/genes.fasta')
-        with open('data/genes.fasta', "rU") as fh:
+        with open('data/genes.fasta', "r") as fh:
             seqio = SeqIO.to_dict(SeqIO.parse(fh, "fasta"))
         assert str(fasta['gi|557361099|gb|KF435150.1|'][:].reverse.complement) == str(seqio['gi|557361099|gb|KF435150.1|'].reverse_complement().seq)
